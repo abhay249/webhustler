1aab4c24ede3d3e5fad3c59bc3fb300a4eff198c
abhay249 <97975372+abhay249@users.noreply.github.com>
2024-03-10 14:29:39 +0530

    db update

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const DB = process.env.DATABASE.replace(
 );
 
 mongoose
-  // .connect(DB, {
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+  // .connect(process.env.DATABASE_LOCAL, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
   })
